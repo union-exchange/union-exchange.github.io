ba392deedb4974567a2807fd01da350b6032b3af
union-exchange <85065690+union-exchange@users.noreply.github.com>
2021-05-30 22:28:18 +0100

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'database_name_here' );
+/**define( 'DB_NAME', 'database_name_here' );
 
 /** MySQL database username */
 define( 'DB_USER', 'username_here' );
@@ -35,7 +35,29 @@ define( 'DB_HOST', 'localhost' );
 define( 'DB_CHARSET', 'utf8' );
 
 /** The Database Collate type. Don't change this if in doubt. */
-define( 'DB_COLLATE', '' );
+define( 'DB_COLLATE', '' ); */
+
+if(isset($_ENV[`CLEARDB_DATABASE_URL`])) {
+    $db = parse_url($_ENV['CLEARDB_DATABASE_URL']);
+    define('DB_NAME', trim($db['heroku_dc68e93a6c6e7d9'],'/'));
+    define('DB_USER', $db['user']);
+    define('DB_PASSWORD', $db['pass']);
+    define('DB_HOST', $db['host']);
+    define('DB_CHARSET', 'utf8');
+    define('DB_COLLATE', '');
+} else {
+    die('No Database credentials!');
+}
+
+
+
+
+
+
+
+
+
+
 
 /**#@+
  * Authentication Unique Keys and Salts.
